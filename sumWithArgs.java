c6c2dde47a7380a689c302e38e765a4438418b29
Java Playground <playground@example.com>
2026-07-13 09:35:02 +0000

    Save snippet: sumWithArgs.java

---
+++ sumWithArgs.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static int Sum(int a, int b){
+    int sum = a+b;
+    return sum;
+  }
+    public static void main(String[] args) {
+      int ans = Sum(3,4);
+      System.out.println(ans);
+      
+    }
+}
