9f8904cdc3c7bff97e3ed7ca7b97fd566923ec73
Java Playground <playground@example.com>
2026-07-15 07:48:48 +0000

    Save snippet: BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] arr = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int ans = Bs(arr, 9);
+    System.out.println("Found at " + ans);
+
+
+  }
+  static int Bs(int[] arr, int target){
+    int start =0;
+    int end = arr.length-1;
+    while (start<=end){
+      int mid = start + (end-start)/2;
+      if(target < arr[mid]){
+        end = mid-1;
+      } else if(target > arr[mid]){
+        start =mid +1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+}
